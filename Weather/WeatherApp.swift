726bfcd1295fe721f9adad5586eb075ee5e658cb
Ramesh Tekumudi <ramesh2014tk@gmail.com>
2023-04-10 16:06:42 +0530

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by RAMESH on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
